77aa427ac568490689dadc05c82e4d680f268f4c
imvenx <imvenx@proton.me>
2024-04-22 16:51:04 +0200

    add trigger sound, add mouse move

--- src/models.ts
+++ src/models.ts
@@ -1,9 +1,19 @@
-import { ArcaneBaseEvent } from "arcanepad-web-sdk"
+import { AEventName, ArcaneBaseEvent } from "arcanepad-web-sdk"
 
 export class AttackedEvent extends ArcaneBaseEvent {
   damage: number
   constructor(damage: number) {
     super('Attacked')
     this.damage = damage
+  }
+}
+
+export class MouseMoveEvent extends ArcaneBaseEvent {
+  x: number
+  y: number
+  constructor(x: number, y: number) {
+    super('_MouseMove');
+    this.x = x
+    this.y = y
   }
 }
--- src/pages/Pads/TutorialPad.vue
+++ src/pages/Pads/TutorialPad.vue
@@ -4,7 +4,7 @@
   background-image: url('/src/assets/images/gun.webp'); background-position: 60% 25%; background-size: 130%; background-repeat: no-repeat;">
 
     <div style="display: grid; gap: 2px; grid-template-columns: 30% 40% 30%;">
-      <div class="gunButton" @touchstart="isMenuEnabled = true">
+      <div class="gunButton" @touchend="openMenu()">
         Menu
       </div>
       <div class="gunButton">
@@ -38,37 +38,70 @@
       </div>
     </div>
     <div v-if="isMenuEnabled"
-      style="position: absolute; background-color: black; width: 100%; height: 100%; padding: 5%; display: grid;">
-      <q-btn size="xl" outline @touchstart="isMenuEnabled = false">Back to Gun</q-btn>
-      <q-btn size="xl" outline @touchend="exitApp()">Exit App</q-btn>
-      <q-btn size="xl" outline>Disable Sound</q-btn>
-      <q-btn size="xl" outline>Disable Vibration</q-btn>
+      style="position: absolute; background-color: black; width: 100%; height: 100%; padding: 5%; display: grid; gap: 5%;">
+      <q-btn size="xl" outline @touchstart="closeMenu()">Back to Gun</q-btn>
+      <q-btn size="xl" outline @click="toggleSound()">{{ isSoundEnabled ? `Sound Enabled` : `Sound
+        Disabled` }}</q-btn>
+      <q-btn size="xl" outline @click="toggleVibration()">{{ isVibrationEnabled ? `Vibration
+        Enabled` : `Vibration
+        Disabled` }}</q-btn>
+      <q-btn size="xl" outline @touchend="exitApp()" color="red">Exit App</q-btn>
     </div>
   </div>
 
 </template>
 
 <script setup lang="ts">
-import { Arcane, ArcaneBaseEvent } from 'arcanepad-web-sdk';
+import { Arcane, ArcaneBaseEvent, GetPointerEvent } from 'arcanepad-web-sdk';
 import { MouseButtonHoldEvent, MouseButtonPressEvent, MouseButtonReleaseEvent } from 'src/models/models';
-import { onMounted, onUpdated, ref } from 'vue';
+import { onMounted, onUnmounted, onUpdated, ref } from 'vue';
 import shootSound from 'assets/sounds/shotgun/shotgun_fire.mp3'
+import triggerSound from 'assets/sounds/shotgun/trigger.wav'
 import reloadSound from 'assets/sounds/shotgun/shotgun_reload.mp3'
 import { playSound } from 'src/utils';
 import reloadSound1 from 'assets/sounds/shotgun/shotgun_reload_1.wav'
+import { MouseMoveEvent } from 'src/models';
 
 const count = ref(0)
 const isSoundEnabled = ref(true)
-const isShootEnabled = ref(true)
+const isShootEnabled = ref(false)
 const isVibrationEnabled = ref(true)
 const isMenuEnabled = ref(false)
 
+let isCalibrateSequenceTopLeft = false
+let isCalibrateSequenceBottomRight = false
+
 onMounted(() => {
   Arcane.pad?.setScreenOrientationLandscape()
+
+  Arcane.pad?.onGetPointer((e: GetPointerEvent) => {
+    emitMoveMouse(e)
+  })
+
+  document.querySelectorAll('.gunButton').forEach(button => {
+    button.addEventListener('touchstart', changeBackgroundColorOnPress);
+    button.addEventListener('touchend', resetBackgroundColorOnRelease);
+  });
+})
+
+function changeBackgroundColorOnPress(event: any) {
+  event.target.style.backgroundColor = 'rgba(0,100,255,.5)';
+}
+
+function resetBackgroundColorOnRelease(event: any) {
+  event.target.style.backgroundColor = '';
+}
+
+onUnmounted(() => {
+  Arcane.pad?.stopGetPointer()
 })
 
 function shoot() {
-  if (!isShootEnabled.value) return
+
+  if (!isShootEnabled.value) {
+    if (isSoundEnabled.value) playSound(triggerSound)
+    return
+  }
 
   Arcane.msg.emit(new MouseButtonHoldEvent('Left'), [])
 
@@ -81,7 +114,10 @@ function stopShoot() {
 }
 
 function reload() {
-  if (!isShootEnabled.value) return
+  if (!isShootEnabled.value) {
+    if (isSoundEnabled.value) playSound(triggerSound)
+    return
+  }
 
   if (isSoundEnabled.value) playSound(reloadSound)
   Arcane.msg.emit(new MouseButtonPressEvent('Right'), [])
@@ -92,12 +128,49 @@ function reload() {
 
 function toggleShootEnabled() {
   isShootEnabled.value = !isShootEnabled.value
+
+  if (isShootEnabled.value) {
+    Arcane.pad?.startGetPointer()
+  } else {
+    Arcane.pad?.stopGetPointer()
+  }
+
   if (isVibrationEnabled.value) Arcane.pad?.vibrate(100)
   if (isSoundEnabled.value) playSound(reloadSound1)
 }
 
 function exitApp() {
+  if (isSoundEnabled.value) playSound(reloadSound1)
   Arcane.msg.emitToViews(new ArcaneBaseEvent('ExitApp'))
+}
+
+const _x = ref(0)
+function emitMoveMouse({ x, y }: GetPointerEvent) {
+  if (!isShootEnabled.value) return
+
+  if (isCalibrateSequenceTopLeft || isCalibrateSequenceBottomRight) return
+  _x.value = x
+  Arcane.msg.emit(new MouseMoveEvent(x, y), [])
+}
+
+function toggleSound() {
+  if (!isSoundEnabled.value) playSound(reloadSound1)
+  isSoundEnabled.value = !isSoundEnabled.value
+}
+
+function toggleVibration() {
+  if (isSoundEnabled.value) playSound(reloadSound1)
+  isVibrationEnabled.value = !isVibrationEnabled.value
+}
+
+function closeMenu() {
+  if (isSoundEnabled.value) playSound(reloadSound1)
+  isMenuEnabled.value = false
+}
+
+function openMenu() {
+  if (isSoundEnabled.value) playSound(reloadSound1)
+  isMenuEnabled.value = true
 }
 
 </script>
