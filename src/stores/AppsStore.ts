8dad9355e918f2381e8d97d1386f6379e7b7bb40
imvenx <imvenx@proton.me>
2024-04-27 11:09:29 +0200

    change descritpions

--- src/stores/AppsStore.ts
+++ src/stores/AppsStore.ts
@@ -10,18 +10,18 @@ export const apps: LightgunApp[] = [
   {
     name: 'MetroCop',
     title: 'Metro Cop',
-    description: 'Support the developer by visiting https://helpcomputer.itch.io/metro-cop. This game is independently developed and is not affiliated with Arcanepad.',
+    description: 'This game is independently developed and is not affiliated with Arcanepad. Support the developer by visiting helpcomputer.itch.io/metro-cop.',
   },
   {
     name: 'RushdownRivalsReloaded',
     title: 'Rushdown Rivals Reloaded',
-    description: 'Support the developer by visiting https://spagato.itch.io/rushdown-reloaded. This game is independently developed and is not affiliated with Arcanepad.',
+    description: 'This game is independently developed and is not affiliated with Arcanepad. Support the developer by visiting spagato.itch.io/rushdown-reloaded.',
     video: 'Rushdown.mp4'
   },
   {
     name: 'HouseOfTheBread',
     title: 'House of the Bread',
-    description: 'Support the developer by visiting https://theprinceofmars.itch.io/the-house-of-the-bread. This game is independently developed and is not affiliated with Arcanepad.',
+    description: 'This game is independently developed and is not affiliated with Arcanepad. Support the developer by visiting theprinceofmars.itch.io/the-house-of-the-bread.',
     video: 'HouseOfTheBread.mp4'
   },
 ]
